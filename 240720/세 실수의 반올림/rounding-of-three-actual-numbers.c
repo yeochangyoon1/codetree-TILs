0c9b243289d2166e1b13306b1b3e20353b2fe686
yeochangyoon1 <162788829+yeochangyoon1@users.noreply.github.com>
2024-07-20 15:25:29 +0900

    Title: 세 실수의 반올림, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240720/세 실수의 반올림/rounding-of-three-actual-numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double a;
+    double b;
+    double c;
+    scanf("%lf %lf %lf", &a, &b, &c);
+    printf("%.3f\n", a);
+    printf("%.3f\n", b);
+    printf("%.3f", c);
+    return 0;
+}
