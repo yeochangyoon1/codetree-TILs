95a5a3fecf90e8e9a973747049e53c1e13cb4053
yeochangyoon1 <162788829+yeochangyoon1@users.noreply.github.com>
2024-07-20 16:06:38 +0900

    Title: 세 수의 합과 평균 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240720/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    int b;
+    int c;
+    scanf("%d %d %d", &a, &b, &c);
+    printf("%d\n%d", a + b + c, (a + b + c) / 3);
+    return 0;
+}
