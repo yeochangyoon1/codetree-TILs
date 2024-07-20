3983593397011568a0541fbc0d487777725824da
yeochangyoon1 <162788829+yeochangyoon1@users.noreply.github.com>
2024-07-20 13:33:50 +0900

    Title: 변수 값 교체하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240720/변수 값 교체하기 3/replacing-variable-values-3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 3;
+    int b = 5;
+    int temp = a;
+    a = b;
+    b = temp;
+    printf("%d\n", a);
+    printf("%d", b);
+    return 0;
+}
