a600f930a3ec160068f649a5bdb56dfce9a1fb59
yeochangyoon1 <162788829+yeochangyoon1@users.noreply.github.com>
2024-07-20 19:49:52 +0900

    Title: 출석 부르기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240720/출석 부르기/calling-attendance.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d", &n);
+
+    if(n == 1)
+    {
+        printf("John");
+    }
+    else if(n == 2)
+    {
+        printf("Tom");
+    }
+    else if(n == 3)
+    {
+        printf("Paul");
+    }
+    else
+    {
+        printf("Vacancy");
+    }
+    return 0;
+}
