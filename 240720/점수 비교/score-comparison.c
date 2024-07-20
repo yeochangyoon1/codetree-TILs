30d3d84f530b91d3ba6e36d3f3afec4d9167c4b7
yeochangyoon1 <162788829+yeochangyoon1@users.noreply.github.com>
2024-07-20 20:46:56 +0900

    Title: 점수 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240720/점수 비교/score-comparison.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+
+    printf("%d\n", (a > c) && (b > d));
+    return 0;
+}
