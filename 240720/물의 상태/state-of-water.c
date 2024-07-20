e4f31241d4ec82f4a726a306694ac09feffbf1da
yeochangyoon1 <162788829+yeochangyoon1@users.noreply.github.com>
2024-07-20 19:39:18 +0900

    Title: 물의 상태, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240720/물의 상태/state-of-water.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d", &n);
+    if(n < 0)
+    {
+        printf("ice");
+    }
+    else if(n > 100)
+    {
+        printf("vapor");
+    }
+    else
+    {
+        printf("water");
+    }
+    return 0;
+}
