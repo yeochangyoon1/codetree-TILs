03442a7c0f13f8e0e5e4f87664b3512459392752
yeochangyoon1 <162788829+yeochangyoon1@users.noreply.github.com>
2024-07-20 15:46:57 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240720/전화번호 바꾸기/change-phone-number.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int xxxx;
+    int yyyy;
+    scanf("010-%d-%d", &xxxx, &yyyy);
+    printf("010-%d-%d", yyyy, xxxx);
+    return 0;
+}
