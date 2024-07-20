815ab1520fa58cbcb220169be9b049836cfad956
yeochangyoon1 <162788829+yeochangyoon1@users.noreply.github.com>
2024-07-20 15:58:07 +0900

    Title: 입력받아 연산하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240720/입력받아 연산하기/take-input-and-operate.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    int b;
+    scanf("%d %d", &a, &b);
+    a += 87;
+    b %= 10;
+    printf("%d\n%d", a, b);
+    return 0;
+}
