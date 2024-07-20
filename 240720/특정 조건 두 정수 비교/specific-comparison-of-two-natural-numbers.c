d360f1a8b260fb32624ace5741ee7fbcd79fdb02
yeochangyoon1 <162788829+yeochangyoon1@users.noreply.github.com>
2024-07-20 20:23:22 +0900

    Title: 특정 조건 두 정수 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240720/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    int b;
+    scanf("%d %d", &a, &b);
+
+    if(a < b)
+    {
+        printf("1");
+    }
+    else
+    {
+        printf("0");
+    }
+
+    printf(" ");
+
+    if(a == b)
+    {
+        printf("1");
+    }
+    else
+    {
+        printf("0");
+    }
+    return 0;
+}
