d191ffbca2d8461aac2d2f097bbba9f4ee2dce1d
yeochangyoon1 <162788829+yeochangyoon1@users.noreply.github.com>
2024-07-21 17:23:46 +0900

    Title: 총합 구하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240721/총합 구하기/find-the-total.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    scanf("%d %d", &a, &b);
+    int sum = 0;
+    for(int i = a; i <= b; i++)
+    {
+        if(i % 6 == 0 && i % 8 != 0)
+        {
+            sum += i;
+        }
+    }
+    printf("%d", sum);
+    return 0;
+}
