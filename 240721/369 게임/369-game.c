d63baea684ee6d5e03c4c3c59ff762d236bc8b76
yeochangyoon1 <162788829+yeochangyoon1@users.noreply.github.com>
2024-07-21 16:15:32 +0900

    Title: 369 게임, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240721/369 게임/369-game.c
+++ 240721/369 게임/369-game.c
@@ -6,7 +6,17 @@ int main() {
     scanf("%d", &n);
     for(int i = 1; i <= n; i++)
     {
-        if(i % 3 == 0 || (i != 10 &&(i % 10) % 3) == 0 || (i > 9 && (i / 10) % 3 == 0))
+        int i1 = i % 10;
+        int i2 = (i / 10) % 10;
+        if(i % 3 == 0)
+        {
+            printf("0 ");
+        }
+        else if(i1 == 3 || i1 == 6 || i1 == 9)
+        {
+            printf("0 ");
+        }
+        else if(i2 == 3 || i2 == 6 || i2 == 9)
         {
             printf("0 ");
         }
