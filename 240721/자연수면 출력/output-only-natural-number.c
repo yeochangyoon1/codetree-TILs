fe4568326fce5a51c36c2b061e39d21ac0b182d0
yeochangyoon1 <162788829+yeochangyoon1@users.noreply.github.com>
2024-07-21 15:07:49 +0900

    Title: 자연수면 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240721/자연수면 출력/output-only-natural-number.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    scanf("%d %d", &a, &b);
+    if(a > 0)
+    {
+        for(int i = 1; i <= b; i++)
+        {
+            printf("%d", a);
+        }
+    }
+    return 0;
+}
