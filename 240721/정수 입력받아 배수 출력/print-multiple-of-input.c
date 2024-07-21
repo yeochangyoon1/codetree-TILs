11256778971e42f9bdb78a2cf25d3b5b4c649bf8
yeochangyoon1 <162788829+yeochangyoon1@users.noreply.github.com>
2024-07-21 13:06:54 +0900

    Title: 정수 입력받아 배수 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240721/정수 입력받아 배수 출력/print-multiple-of-input.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d", &n);
+    for(int i = 1; i <= 5; i++)
+    {
+        printf("%d ", i * n);
+    }
+    return 0;
+}
