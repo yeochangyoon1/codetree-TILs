b64148dc78b96fa0e57c67d11626143010d63cd9
yeochangyoon1 <162788829+yeochangyoon1@users.noreply.github.com>
2024-07-22 16:05:44 +0900

    Title: 0 나올때까지 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240722/0 나올때까지 출력/print-until-zero-is-given.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    while(1)
+    {
+        int n;
+        scanf(" %d", &n);
+        if(n == 0)
+        {
+            break;
+        }
+        printf("%d\n", n);
+    }
+    return 0;
+}
