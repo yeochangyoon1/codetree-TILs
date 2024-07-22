37e6e1867b0c713db6add4853272403ed782fb1f
yeochangyoon1 <162788829+yeochangyoon1@users.noreply.github.com>
2024-07-22 20:39:45 +0900

    Title: 별표 출력하기 6, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240722/별표 출력하기 6/print-star-6.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d", &n);
+    for(int i = n; i >= 1; i--)
+    {
+        for(int j = 1; j <= n - i; j++)
+        {
+            printf("  ");
+        }
+        for(int j = 1; j <= 2 * i - 1; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    for(int i = 2; i <= n; i++)
+    {
+        for(int j = 1; j <= n - i; j++)
+        {
+            printf("  ");
+        }
+        for(int j = 1; j <= 2 * i - 1; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
