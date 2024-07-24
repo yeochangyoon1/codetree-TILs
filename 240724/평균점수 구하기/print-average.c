8e637bd019e7fcbac2915044be092aef55825f4c
yeochangyoon1 <162788829+yeochangyoon1@users.noreply.github.com>
2024-07-24 22:38:25 +0900

    Title: 평균점수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240724/평균점수 구하기/print-average.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double score[8];
+    double sum = 0;
+    for(int i = 0; i < 8; i++)
+    {
+        scanf(" %lf", &score[i]);
+        sum += score[i];
+    }
+    double avg = sum / 8;
+    printf("%.1f", avg);
+    return 0;
+}
