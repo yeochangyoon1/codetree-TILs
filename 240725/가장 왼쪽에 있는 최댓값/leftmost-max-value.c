b0452ae44df893dac33807b57281c8d3bd7c626c
yeochangyoon1 <162788829+yeochangyoon1@users.noreply.github.com>
2024-07-25 22:19:05 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240725/가장 왼쪽에 있는 최댓값/leftmost-max-value.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d", &n);
+    int number[1000];
+    int count[1001] = {0};
+    for(int i = 0; i < n; i++)
+    {
+        scanf(" %d", &number[i]);
+        count[number[i]]++;
+    }
+    int max[1000];
+    int index = n;
+    for(int i = 0; i < n; i++)
+    {   
+        max[i] = number[0];
+        for(int j = index - 2; j >= 0; j--)
+        {
+            if(max[i] <= number[j])
+            {
+                max[i] = number[j];
+                index = j + 1;
+            }
+        }
+        printf("%d ", index);
+        if(number[max[i]] == 1 || index == 1)
+        {
+            break;
+        }
+    }
+    return 0;
+}
