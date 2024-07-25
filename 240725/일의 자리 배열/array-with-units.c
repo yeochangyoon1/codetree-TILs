546c1816b4bb40258e67df9d386eb0d1d5d1471d
yeochangyoon1 <162788829+yeochangyoon1@users.noreply.github.com>
2024-07-25 16:41:19 +0900

    Title: 일의 자리 배열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240725/일의 자리 배열/array-with-units.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n[10];
+    scanf("%d %d", &n[0], &n[1]);
+    for(int i = 2; i < 10; i++)
+    {
+        n[i] = (n[i - 1] + n[i - 2]) % 10;
+    }
+    for(int i = 0; i < 10; i++)
+    {
+        printf("%d ", n[i]);
+    }
+    return 0;
+}
