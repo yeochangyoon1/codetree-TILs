8c58742952b48975f90ef591e55e948e6532160c
yeochangyoon1 <162788829+yeochangyoon1@users.noreply.github.com>
2024-08-01 14:50:35 +0900

    Title: 단어로 구분하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240801/단어로 구분하기 3/separate-words-with-words-3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char str[10][100];
+    for(int i = 0; i < 10; i++)
+    {
+        scanf("%s", str[i]);
+    }
+    for(int i = 9; i >= 0; i--)
+    {
+        printf("%s\n", str[i]);
+    }
+    return 0;
+}
