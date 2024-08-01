ce9dc80e6cbe4145fe9e31bb921e5f90167d4c11
yeochangyoon1 <162788829+yeochangyoon1@users.noreply.github.com>
2024-08-01 15:51:53 +0900

    Title: Run Length 인코딩, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240801/Run Length 인코딩/run-length-encoding.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char str1[1001];
+    scanf("%s", str1);
+    int len = strlen(str1);
+    int count[1000];
+    for(int i = 0; i < 1000; i++)
+    {
+        count[i] = 1;
+    }
+    int n1 = 0;
+    for(int i = 1; i < len; i++)
+    {
+        if(str1[i] == str1[i - 1])
+        {
+            count[n1]++;
+        }
+        else
+        {
+            n1++;
+        }
+    }
+    char str2[2001];
+    int n2 = 0;
+    n1 = 0;
+    for(int i = 0; i < len; i++)
+    {
+        if(str1[i] == str1[i + 1])
+        {
+            str2[n2] = str1[i];
+        }
+        else
+        {
+            n2++;
+            str2[n2] = count[n1] + '0';
+            n1++;
+            n2++;
+            str2[n2] = str1[i + 1];
+        }
+    }
+    int len2 = strlen(str2);
+    printf("%d\n%s", len2, str2);
+    return 0;
+}
