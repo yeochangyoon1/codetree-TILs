98f17f4e694e223efe5fe12173d126e6bc4d720c
yeochangyoon1 <162788829+yeochangyoon1@users.noreply.github.com>
2024-08-02 17:47:32 +0900

    Title: 아스키코드표 맞추기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240802/아스키코드표 맞추기/chart-of-ASCII.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c, d, e;
+    scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+    printf("%c %c %c %c %c", a, b, c, d, e);
+    return 0;
+}
