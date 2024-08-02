ea5e359849d6fb0c57ab838282aca5d4f5705c8d
yeochangyoon1 <162788829+yeochangyoon1@users.noreply.github.com>
2024-08-02 20:43:31 +0900

    Title: 미는 횟수, Time: 1048ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240802/미는 횟수/number-of-pushes.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char str1[101];
+    char str2[101];
+    scanf("%s %s", str1, str2);
+    int n = 0;
+    int len = strlen(str1);
+    while(1)
+    {
+        if(!strcmp(str1, str2))
+        {
+            printf("%d", n);
+            break;
+        }
+        char temp = str1[len - 1];
+        for(int i = len - 1; i > 0; i--)
+        {
+            str1[i] = str1[i - 1];
+        }
+        str1[0] = temp;
+        n++; 
+    }
+    return 0;
+}
